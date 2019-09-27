52a35f9a51d2d08bdea03a0225633bcfd550ead2
junaidtaj <junaid.taj@slock.it>
2019-09-27 17:03:09 +0200

    generate docker using settings

--- src/Containers/SettingsContainer.js
+++ src/Containers/SettingsContainer.js
@@ -3,6 +3,7 @@ import SettingsComponent from '../Components/SettingsComponent';
 import ethWallet from 'ethereumjs-wallet';
 import DialogComponent from '../Components/DialogComponent'
 import defaultConfig from '../defaultConfig'
+import path from 'path'
 
 export default class SettingsContainer extends Component {
     constructor(props) {
@@ -19,6 +20,7 @@ export default class SettingsContainer extends Component {
             privatekey: '',
             keystorepath: '',
             keyphrase: '',
+
             capabilities: '',
             deposit: '',
             timeout: '',
@@ -26,23 +28,22 @@ export default class SettingsContainer extends Component {
             outputData: ''
         }
 
+        this.NW = {
+            "Mainnet": "0x1",
+            "Kovan": "0x2a",
+            "Goerli": "0x5"
+        };
     }
+
 
     handleChange = (e) => {
         const { id, value } = e.target;
         let newState = Object.assign({}, this.props);
 
-        if (e.target.type == 'radio'){
-            var nw = '0x1';
-            if(value=='Kovan'){
-                nw="0x2a";
-            }
-            else if(value=='Goerli'){
-                nw="0x5";
-            }
-
-            newState["noderegistry"]=defaultConfig.servers[nw].contract;
-            newState[e.target.name] = value;}
+        if (e.target.type == 'radio') {
+            newState["noderegistry"] = defaultConfig.servers[this.NW[value]].contract;
+            newState[e.target.name] = value;
+        }
         else
             newState[id] = value;
 
@@ -50,35 +51,52 @@ export default class SettingsContainer extends Component {
     }
 
     generateConfig = (e) => {
-        var dockerConf="version: '2'\n\
+        var dockerConf = "\n\
+        version: '2'\n\
         services:\n\
           incubed-server:\n\
             image: slockit/in3-server:latest\n";
 
-        if(this.state.keystorepath){
-            dockerConf+="volumes:\n\
-            - $PWD/keys:"+(this.state.keystorepath)+"               # directory where the private key is stored";
-        }
-        else{}
+        if (this.state.keystorepath && this.state.keyphrase) {
 
-        dockerConf+="\n\
+            dockerConf += "volumes:\n\
+            - "+ path.dirname(this.state.keystorepath) + ":/secure                                     # directory where the private key is stored";
+        }
+
+        dockerConf += "\n\
               ports:\n\
             - 8500:8500/tcp                                         # open the port 8500 to be accessed by the public\n\
+            commands:\n";
+
+        if (this.state.keystorepath && this.state.keyphrase) {
+            dockerConf +=
+                "- --privateKey=/secure/" + (this.state.keystorepath.replace(/^.*[\\\/]/, '')) + "                       # internal path to the key\n\
+            - --privateKeyPassphrase="+ (this.state.keyphrase) + "                          # passphrase to unlock the key\n";
+        }
+        else {
+            dockerConf += "- --privateKey=" + this.state.privatekey;
+        }
+
+        dockerConf += "\n\
             command:\n\
-            - --privateKey=/secure/myKey.json                       # internal path to the key\n\
-            - --privateKeyPassphrase=dummy                          # passphrase to unlock the key\n\
-            - --chain=0x1                                           # chain (Kovan)\n\
+            - --chain="+ this.NW[this.state.network] + "                                           # chain (Kovan)\n\
             - --rpcUrl=http://incubed-parity:8545                   # URL of the Kovan client\n\
-            - --registry=0xFdb0eA8AB08212A1fFfDB35aFacf37C3857083ca # URL of the Incubed registry\n\
-            - --autoRegistry-url=http://in3.server:8500             # check or register this node for this URL\n\
-            - --autoRegistry-deposit=2                              # deposit to use when registering\n\
-                        \n\
-          incubed-parity:\n\
-            image: slockit/parity-in3:v2.2                          # parity-image with the getProof-function implemented\n\
-            command:\n\
-            - --auto-update=none                                    # do not automatically update the client\n\
-            - --pruning=archive \n\
-            - --pruning-memory=30000                                # limit storage";
+            - --registry="+ this.state.noderegistry + " # URL of the Incubed registry\n";
+
+        if (this.state.orgname) dockerConf += "            - --profile-name=" + this.state.orgname + "\n";
+        if (this.state.profileicon) dockerConf += "            - --profile-icon=" + this.state.profileicon + "\n";
+        if (this.state.description) dockerConf += "            - --profile-comment=" + this.state.description + "\n";
+        if (this.state.orgurl) dockerConf += "            - --profile-url=" + this.state.orgurl + "\n";
+        if (this.state.logslevel) dockerConf += "            - --logging-level=" + this.state.logslevel + "\n";
+        if (this.state.blockheight) dockerConf += "            - --minBlockHeight=" + this.state.blockheight + "\n";
+
+        dockerConf += "\n\
+        incubed - parity: \n\
+        image: slockit / parity - in3: v2.2                          # parity - image with the getProof - function implemented\n\
+        command: \n\
+        - --auto-update=none                                    # do not automatically update the client\n\
+        - --pruning=archive \n\
+        - --pruning-memory=30000                                # limit storage";
 
         //const { id, value } = e.target;
         //alert(this.state.logslevel);
