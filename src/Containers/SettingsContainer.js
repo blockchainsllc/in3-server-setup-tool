83284b5d4f10dd01e80727a7fe22c5eda12114ac
junaidtaj <junaid.taj@slock.it>
2020-01-11 22:04:37 +0100

    settings component respinsible only for docker-compose generating

--- src/Containers/SettingsContainer.js
+++ src/Containers/SettingsContainer.js
@@ -67,15 +67,6 @@ export default class SettingsContainer extends Component {
             keyphrase2: '',
             keyexported: false,
 
-            capproof: true,
-            caparchive: false,
-            caphttp: false,
-            caponion: false,
-            capbinary: false,
-            capstats: false,
-
-            deposit: '10000000000000000',
-            in3nodeurl: '',  //Math.random().toString(36).substring(7),
             outputData: '',
 
             ethnodeurl: '',
@@ -110,7 +101,7 @@ export default class SettingsContainer extends Component {
             newState[id] = value;
         }
 
-        this.setState(newState);
+        this.setState(newState, () => {this.props.dataChanged(this.state)});
     }
 
     generateConfig = (e) => {
@@ -197,7 +188,7 @@ export default class SettingsContainer extends Component {
 
         let newState = Object.assign({}, this.props);
         newState['outputData'] = dockerConf;
-        this.setState(newState);
+        this.setState(newState, () => {this.props.dataChanged(this.state)});
     }
 
     generatePrivateKey = (e) => {
@@ -222,103 +213,7 @@ export default class SettingsContainer extends Component {
             newState.privatekey = wallet.getPrivateKeyString();
             newState.address = wallet.getAddressString();
         }
-        this.setState(newState);
-    }
-
-    signForRegister = (url, props, weight, owner, pk) => {
-
-        const msgHash = ethUtil.keccak(
-            Buffer.concat([
-                in3Common.serialize.bytes(url),
-                in3Common.util.toBuffer(props, 24),
-                in3Common.serialize.uint64(weight),
-                in3Common.serialize.address(owner)
-            ])
-        )
-        const msgHash2 = ethUtil.keccak(in3Common.util.toHex("\x19Ethereum Signed Message:\n32") + in3Common.util.toHex(msgHash).substr(2))
-        const s = ethUtil.ecsign((msgHash2), in3Common.serialize.bytes32(pk))
-
-        return {
-            ...s,
-            address: in3Common.util.getAddress(pk),
-            msgHash: in3Common.util.toHex(msgHash2, 32),
-            signature: in3Common.util.toHex(s.r) + in3Common.util.toHex(s.s).substr(2) + in3Common.util.toHex(s.v).substr(2),
-            r: in3Common.util.toHex(s.r),
-            s: in3Common.util.toHex(s.s),
-            v: s.v
-        }
-    }
-
-    mintERC20 = (erc20Contract, fromAddr, amount) => {
-        return erc20Contract.methods.mint().send({
-            from: fromAddr,
-            to: erc20Contract.address,
-            value: amount
-            , gas: '300000'
-        }).on('transactionHash', function (hash) {
-            console.log("Transaction Hash: " + hash);
-        })
-            .on('confirmation', function (confirmationNumber, receipt) {
-                console.log("Confirmation Num: " + JSON.stringify(confirmationNumber) + " Receipt: " + receipt)
-            })
-            .on('receipt', function (receipt) {
-                console.log("Receipt: " + JSON.stringify(receipt));
-                return receipt;
-            })
-        // .on('error', function (err) {
-        //     console.log(err.message)
-        //     return err;
-        // });
-    }
-
-    sendRegisterNode = (registryContract, url, props, signer, weight, deposit, signature, txSender, nodeRegistryAddr) => {
-
-        return registryContract.methods
-            .registerNodeFor(
-                url, props, signer, weight, deposit, signature.v, signature.r, signature.s)
-            .send({
-                from: txSender,
-                to: nodeRegistryAddr,
-                value: 0
-                //,gas: '30000'
-            }).on('transactionHash', function (hash) {
-                console.log("Transaction Hash: " + hash);
-            })
-            .on('confirmation', function (confirmationNumber, receipt) {
-                console.log("Confirmation Num: " + JSON.stringify(confirmationNumber) + " Receipt: " + receipt)
-            })
-            .on('receipt', function (receipt) {
-                console.log("Receipt: " + JSON.stringify(receipt));
-
-                return receipt
-            });
-        // .on('error', function (err) {
-        //     this.showMessage(err)
-        //     return err
-        // });
-    }
-
-    sendApprove = (amount, fromAddr, toAddr, erc20Contract) => {
-        return erc20Contract.methods.approve(toAddr, amount).send({
-            from: fromAddr,
-            to: erc20Contract.address,
-            value: 0
-            //,gas: '30000'
-        })
-            .on('transactionHash', function (hash) {
-                console.log("Transaction Hash: " + hash);
-            })
-            .on('confirmation', function (confirmationNumber, receipt) {
-                console.log("Confirmation Num: " + confirmationNumber + " Receipt: " + receipt)
-            })
-            .on('receipt', function (receipt) {
-                console.log("Receipt: " + JSON.stringify(receipt));
-                return receipt;
-            });
-        // .on('error', function (err) {
-        //     console.log(JSON.stringify(err))
-        //     return err;
-        // });
+        this.setState(newState, () => {this.props.dataChanged(this.state)});
     }
 
     showMessage(str) {
@@ -332,173 +227,6 @@ export default class SettingsContainer extends Component {
         let newState = Object.assign({}, this.props);
         newState['showProgressBar'] = show;
         this.setState(newState);
-    }
-
-    sendRegTransaction = (web3, window) => {
-        this.showProgress(true);
-
-        if (this.state.deposit === "" || (! /^\d*\.?\d*$/.test(this.state.deposit))) {
-            this.showMessage("Invalid deposit value");
-            this.showProgress(false);
-            return;
-        }
-
-        const url = this.state.in3nodeurl;
-        if (url === "") {
-            this.showMessage("IN3 node URL cannot be empty");
-            this.showProgress(false);
-            return;
-        }
-
-        const PK = this.state.privatekey;
-        if (PK === '') {
-            this.showMessage("First generate private key.");
-            this.showProgress(false);
-            return;
-        }
-
-        if (!this.state.keyexported) {
-            this.showMessage("Please export encrypted private key first.");
-            this.showProgress(false);
-            return;
-        }
-
-        const nodeRegistryAddr = this.state.noderegistry;
-        const nodeRegistryContract = new web3.eth.Contract(NodeRegistry.abi, nodeRegistryAddr);
-
-        const weight = web3.utils.toHex(1);
-
-        let props = web3.utils.toHex(
-            (((this.state.capproof ? 1 : 0) + (this.state.caparchive ? 4 : 0)
-            + (this.state.caphttp ? 8 : 0) + (this.state.caponion ? 20 : 0)
-            + (this.state.capbinary ? 10 : 0) + (this.state.capstats ? 100 : 0)) * 2147483648) 
-            + this.state.blockheight)
-
-        const deposit = this.state.deposit //web3.utils.toHex(Web3.utils.toWei(this.state.deposit, 'ether'));
-        const signature = this.signForRegister(url, props, weight, window.web3.currentProvider.selectedAddress, PK);
-
-        nodeRegistryContract.methods.supportedToken().call().then((erc20Addr) => {
-
-            const erc20Contract = new web3.eth.Contract(ERC20.abi, erc20Addr)
-
-            erc20Contract.methods.balanceOf(window.web3.currentProvider.selectedAddress).call().then((balance) => {
-
-                if (parseInt(balance) < parseInt(deposit)) {
-                    this.showMessage("Insufficient erc20 funds (" + balance + ") for deposit, first converting " + deposit + " wei to erc20")
-
-                    this.mintERC20(
-                        erc20Contract,
-                        window.web3.currentProvider.selectedAddress,
-                        deposit - balance
-                    ).then((res) => {
-
-                        this.sendApprove(
-                            deposit,
-                            window.web3.currentProvider.selectedAddress,
-                            nodeRegistryAddr,
-                            erc20Contract
-                        ).then((res) => {
-
-                            this.sendRegisterNode(
-                                nodeRegistryContract,
-                                url,
-                                props,
-                                this.state.address,
-                                weight,
-                                deposit,
-                                signature,
-                                window.web3.currentProvider.selectedAddress,
-                                nodeRegistryAddr).then((res) => {
-                                    this.showMessage("Registration Completed Successfully")
-                                })
-                        }).catch(
-                            err => {
-                                this.showMessage("Some Error Occured. " + err.message);
-                                this.showProgress(false);
-                            }
-                        )
-                    }).catch(
-                        err => {
-                            this.showMessage("Some Error Occured. " + err.message);
-                            this.showProgress(false);
-                        }
-                    )
-                }
-                else {
-                    this.sendApprove(
-                        deposit,
-                        window.web3.currentProvider.selectedAddress,
-                        nodeRegistryAddr,
-                        erc20Contract).then((res) => {
-
-                            this.sendRegisterNode(
-                                nodeRegistryContract,
-                                url,
-                                props,
-                                this.state.address,
-                                weight,
-                                deposit,
-                                signature,
-                                window.web3.currentProvider.selectedAddress,
-                                nodeRegistryAddr)
-                                .then((res) => {
-
-                                    this.showMessage("Registration Completed Successfully");
-                                    this.showProgress(false);
-
-                                }).catch(
-                                    err => {
-                                        this.showMessage("Some Error Occured. " + err.message);
-                                        this.showProgress(false);
-                                    })
-
-                        }).catch(
-                            err => {
-                                this.showMessage("Some Error Occured. " + err.message);
-                                this.showProgress(false);
-                            }
-                        )
-                }
-            }).catch(
-                err => {
-                    this.showMessage("Some Error Occured. " + err.message);
-                    console.log("Some Error Occured." + JSON.stringify(err));
-                    this.showProgress(false);
-                }
-            )
-        }).catch(
-            err => {
-                this.showMessage("Some Error Occured. " + err.message);
-                console.log("Some Error Occured." + JSON.stringify(err));
-                this.showProgress(false);
-            }
-        )
-    }
-
-    registerin3 = () => {
-        // Modern DApp Browsers
-        if (window.ethereum) {
-            var web3 = new Web3(window.ethereum);
-            try {
-                window.ethereum.enable().then(() => {
-
-                    this.sendRegTransaction(web3, window);
-                });
-            } catch (e) {
-                // User has denied account access to DApp...
-                this.showMessage('You Denied MetaMask Access!');
-            }
-        }
-        // Legacy DApp Browsers
-        else if (window.web3) {
-            var web3 = new Web3(window.web3.currentProvider);
-            this.sendRegTransaction(web3, window);
-        }
-        // Non-DApp Browsers
-        else {
-            this.showMessage('You have to install MetaMask !');
-        }
-
     }
 
     handleClose = () => {
@@ -535,7 +263,23 @@ export default class SettingsContainer extends Component {
             newState.keyexported = true;
         }
 
+        this.setState(newState, () => {this.props.dataChanged(this.state)});
+    }
+
+    //retain state on tab coming back
+    setData = (data) => {
+        let newState = Object.assign({}, this.props);
+
+        for (var prop in data) {
+            if (Object.prototype.hasOwnProperty.call(data, prop) && prop!="dataChanged") {
+                newState[prop] = data[prop];
+            }
+        }
         this.setState(newState);
+    }
+
+    componentDidMount(){
+        this.setData(this.props.downData)
     }
 
     render() {
@@ -558,16 +302,6 @@ export default class SettingsContainer extends Component {
                     keystorepath={this.state.keystorepath}
                     keyphrase1={this.state.keyphrase1}
                     keyphrase2={this.state.keyphrase2}
-
-                    capproof={this.state.capproof}
-                    caparchive={this.state.caparchive}
-                    caphttp={this.state.caphttp}
-                    caponion={this.state.caponion}
-                    capbinary={this.state.capbinary}
-                    capstats={this.state.capstats}
-
-                    deposit={this.state.deposit}
-                    in3nodeurl={this.state.in3nodeurl}
 
                     registerin3={this.registerin3}
 
