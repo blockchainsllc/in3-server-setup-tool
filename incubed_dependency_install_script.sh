f4ce3dd7b0d94117660c00e8a166d980872f65be
pari <parithosh@slock.it>
2020-01-14 12:14:51 +0100

    update with command to run parity

--- incubed_dependency_install_script.sh
+++ incubed_dependency_install_script.sh
@@ -25,8 +25,13 @@ usermod -aG docker $USER
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 # set the right permissions
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
+# create a new screen
+screen
+
+# run incubed-parity
+docker-compose up incubed-parity
 
 
 
